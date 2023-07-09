48c8d27e9ae89dfe903da7583f557f17e755a4e4
ABDULAZEEZ MDKAMIL OLAIDE <kam4love07@gmail.com>
2023-07-09 10:58:11 +0100

    Lesson Four
    How to create components and the use of slot

--- App.vue
+++ App.vue
@@ -59,25 +59,38 @@ let app3 = {
 };
 // const lessonThree = Vue.createApp(app3).mount("#lesson-three");
 
-//Lesson 4
-let app4 = {
-        data() {
-            return {
-                assignments: [
-                    {name: 'Finish Project', complete: false, id: 1},
-                    {name: 'Read Chapter 4', complete: false, id: 2},
-                    {name: 'Turn in Homework', complete: false, id: 3}
-                ]
+//Lesson 4: How to create a Custom Components, and make content dynamic using slot
+let app = {
+    components: {
+        //Registering a component
+        'app-button': {
+            // Disabled button if processing is true
+            template: `
+                <button @click="processing = true" class="bg-gray-200 bg-gray hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+                    <slot />
+                </button>
+            `,
+            data() {
+                return {
+                    processing: false
+                }
             }
-    },
-    computed: {
-        inProgressAssignment() {
-            return this.assignments.filter(a => !a.complete);
         },
-        completedAssignments() {
-            return this.assignments.filter(a => a.complete);
-        } 
-    },
+        'go-button': {
+            // Disabled button if processing is true
+            template: `
+                <button @click="processing = true" class="bg-gray-200 bg-gray hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+                    <slot />
+                </button>
+            `,
+            data() {
+                return {
+                    processing: false
+                }
+            }
+        }
+    }
 };
-const lessonFour = Vue.createApp(app4).mount("#lesson-four");
+
+const lessonFour = Vue.createApp(app).mount("#lesson-four");
 
