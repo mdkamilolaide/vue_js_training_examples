b032b813475767fbded18ec6a2e9f104243e3e0c
kam4love07 <abdulazeezmdkamil@gmail.com>
2023-07-21 11:36:02 +0100

    Lesson 6: How to use Props  If we want our Vue components to be as flexible as possible, we should figure out a way to pass in data from the outside. This way, each 'instance' of a component can be configurable. Props allow for this!

---
+++ lesson_six/components/AppButton.js
@@ -0,0 +1,27 @@
+export default {
+  // Disabled button if processing is true
+  template: `
+        <button 
+            :class="{
+                'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+                'bg-blue-400 bg-gray hover:bg-blue-200': type === 'primary',
+                'bg-purple-400 bg-gray hover:bg-purple-200': type === 'secondary',
+                'bg-gray-200 bg-gray hover:bg-gray-400': type === 'muted',
+                'is-loading': processing
+            }"
+            :disabled="processing"
+        >
+            <slot />
+        </button>
+    `,
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+    processing: {
+      type: Boolean,
+      default: false
+    },
+  },
+};
